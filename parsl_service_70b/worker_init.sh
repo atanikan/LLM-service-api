10b3363660b2eb71d22a7d8a6e674aa7f9084e88
Venkat Vishwanath <venkat@anl.gov>
2023-08-18 20:05:17 -0500

    Update worker_init.sh

--- parsl_service_70b/worker_init.sh
+++ parsl_service_70b/worker_init.sh
@@ -1,4 +1,3 @@
-source ~/.bashrc
 export HF_HOME=/lus/gila/projects/Aurora_deployment/anl_llama/model_weights/huggingface/llama2
 export HF_DATASETS_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
@@ -16,6 +15,8 @@ export FI_MR_CACHE_MONITOR=disabled
 export CCL_ZE_CACHE_OPEN_IPC_HANDLES_THRESHOLD=32768
 export I_MPI_ROOT=/opt/cray/pe/pals/1.2.12/bin/mpiexec
 module use -a /home/ftartagl/modulefiles
+module unload oneapi
 module load oneapi-testing/2023.2.003.PUBLIC_IDP49422oneapi-testing/2023.2.003.PUBLIC_IDP49422
 source /home/jmitche1/70Bccl/libraries.performance.communication.oneccl/build/_install/env/vars.sh
+source /soft/datascience/conda-2023-01-31/miniconda3/bin/activate
 conda activate /lus/gila/projects/Aurora_deployment/conda_env_llm/anl_llma-70b
