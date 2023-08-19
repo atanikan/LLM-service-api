b2e3e5a5a94025ce7a89fb1ae732522afd09a0f8
Venkat Vishwanath <venkat@anl.gov>
2023-08-18 20:01:31 -0500

    Update worker_init.sh

--- parsl_service_13b/worker_init.sh
+++ parsl_service_13b/worker_init.sh
@@ -1,4 +1,4 @@
-source ~/.bashrc
+source /soft/datascience/conda-2023-01-31/miniconda3/bin/activate
 conda activate /lus/gila/projects/Aurora_deployment/conda_env_llm/anl_llma-13b
 export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1 
 export ENABLE_SDP_FUSION=1
